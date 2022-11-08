a86a056aa007fee7b9fbde4adce7b9307adb69ed
Hintz, Steven <STEVEN_HINTZ@homedepot.com>
2022-11-08 15:16:47 -0500

    New spring boot initialized project with added gcp application configurations. (#2)

---
+++ src/main/java/com/homedepot/mm/pc/merchantalerting/MerchantAlertingApplication.java
@@ -0,0 +1,13 @@
+package com.homedepot.mm.pc.merchantalerting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MerchantAlertingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MerchantAlertingApplication.class, args);
+	}
+
+}
