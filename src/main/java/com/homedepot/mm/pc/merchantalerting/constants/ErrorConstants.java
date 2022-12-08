9d333c79411621ba162aade466e83e676a323064
Pradhan, Praney <PRANEY_PRADHAN@homedepot.com>
2022-12-08 12:34:12 -0600

    Update ErrorConstants.java

--- src/main/java/com/homedepot/mm/pc/merchantalerting/constants/ErrorConstants.java
+++ src/main/java/com/homedepot/mm/pc/merchantalerting/constants/ErrorConstants.java
@@ -1,6 +1,6 @@
 package com.homedepot.mm.pc.merchantalerting.constants;
 
-public class ErrorConstants {
-
+public class ErrorConstants 
+{
     public static final String NO_RECORDS_FOR_GIVEN_INPUT = "No records for given input";
 }
