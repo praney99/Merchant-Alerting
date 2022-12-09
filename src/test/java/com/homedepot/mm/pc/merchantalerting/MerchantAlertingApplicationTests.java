2acd00bc2fc61c55ffce4bbb1f0c0efc98f021b5
Pradhan, Praney <PRANEY_PRADHAN@homedepot.com>
2022-12-09 15:53:22 -0600

    Update MerchantAlertingApplicationTests.java

--- src/test/java/com/homedepot/mm/pc/merchantalerting/MerchantAlertingApplicationTests.java
+++ src/test/java/com/homedepot/mm/pc/merchantalerting/MerchantAlertingApplicationTests.java
@@ -5,7 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MerchantAlertingApplicationTests {
+class MerchantAlertingApplicationTests 
+{
 
 	@Test
 	void contextLoads() {
