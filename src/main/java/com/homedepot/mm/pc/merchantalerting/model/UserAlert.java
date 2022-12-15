52de5c25a53cbfca51894a7857b269f31322af26
Hintz, Steven (US) <STEVEN_HINTZ@homedepot.com>
2022-12-15 17:13:58 -0500

    Use composite primary key on user_alert.

--- src/main/java/com/homedepot/mm/pc/merchantalerting/model/UserAlert.java
+++ src/main/java/com/homedepot/mm/pc/merchantalerting/model/UserAlert.java
@@ -1,55 +1,28 @@
 package com.homedepot.mm.pc.merchantalerting.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.sql.Date;
 import java.util.UUID;
 
 @Entity
+@IdClass(UserAlertId.class)
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "user_alert", schema = "alerting_schema")
 public class UserAlert {
 
-
-    @Column(name = "id")
-    private long id;
+    @Id
     @Column(name = "ldap")
     private String ldap;
+    @Id
     @Column(name = "alert_id")
     private UUID alertId;
     @Column(name = "dismiss_date")
     private Date dismissDate;
-
-
-    public UserAlert() {
-    }
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    public long getId() {
-        return id;
-    }
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getLdap() {
-        return ldap;
-    }
-    public void setLdap(String ldap) {
-        this.ldap = ldap;
-    }
-
-    public UUID getAlertId() {
-        return alertId;
-    }
-    public void setAlertId(UUID alertId) {
-        this.alertId = alertId;
-    }
-
-    public Date getDismissDate() {
-        return dismissDate;
-    }
-    public void setDismissDate(Date dismissDate){
-        this.dismissDate = dismissDate;
-    }
 
 }
