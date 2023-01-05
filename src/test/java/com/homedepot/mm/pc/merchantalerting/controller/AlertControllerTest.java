f2ac808e1370619b3b669c69a0aac23b93b07081
Benjamin Bohannon <bcb44jc>
2023-01-05 13:26:57 -0500

    Fixed wrong import, turned off identity for testing + local, and incremented patch.

--- src/test/java/com/homedepot/mm/pc/merchantalerting/controller/AlertControllerTest.java
+++ src/test/java/com/homedepot/mm/pc/merchantalerting/controller/AlertControllerTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.web.client.RestTemplate;
