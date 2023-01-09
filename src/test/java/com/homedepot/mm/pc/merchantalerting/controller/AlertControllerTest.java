bf1482ffe4de2da2cf94bfd59d824c4478857548
Pradhan, Praney <PRANEY_PRADHAN@homedepot.com>
2023-01-09 10:32:26 -0600

    Test from main

--- src/test/java/com/homedepot/mm/pc/merchantalerting/controller/AlertControllerTest.java
+++ src/test/java/com/homedepot/mm/pc/merchantalerting/controller/AlertControllerTest.java
@@ -2,35 +2,19 @@ package com.homedepot.mm.pc.merchantalerting.controller;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import com.homedepot.mm.pc.merchantalerting.PostgresContainerBaseTest;
 import com.homedepot.mm.pc.merchantalerting.domain.CreateAlertRequest;
-import com.homedepot.mm.pc.merchantalerting.exception.ValidationException;
 import com.homedepot.mm.pc.merchantalerting.model.Alert;
 import com.homedepot.mm.pc.merchantalerting.model.UserAlert;
 import com.homedepot.mm.pc.merchantalerting.model.UserAlertId;
-import com.homedepot.mm.pc.merchantalerting.repository.AlertRepository;
-import com.homedepot.mm.pc.merchantalerting.repository.UserAlertRepository;
-import com.homedepot.mm.pc.merchantalerting.service.AlertService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.context.WebApplicationContext;
-
 
 import java.sql.Timestamp;
 import java.util.HashMap;
@@ -39,26 +23,10 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.testcontainers.shaded.org.hamcrest.Matchers.is;
 
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class AlertControllerTest extends PostgresContainerBaseTest
-{
-    @Autowired
-    private WebApplicationContext context;
-    @MockBean
-    private AlertService alertService;
-    @Autowired
-    UserAlertRepository userAlertRepository;
-    @Autowired
-    AlertRepository alertRepository;
-
+public class AlertControllerTest extends PostgresContainerBaseTest {
 
     @LocalServerPort
     private int port;
@@ -66,14 +34,6 @@ public class AlertControllerTest extends PostgresContainerBaseTest
     @Autowired
     @Qualifier("noErrorRestTemplate")
     RestTemplate restTemplate;
-
-    @BeforeEach
-    public void setUp() {
-        mvc = MockMvcBuilders.webAppContextSetup(this.context).build();
-    }
-
-    private MockMvc mvc;
-    private final ObjectMapper mapper= new ObjectMapper();
 
     @Test
     void generateAlertByLdap() {
@@ -183,8 +143,7 @@ public class AlertControllerTest extends PostgresContainerBaseTest
         alert.setTemplateBody(mapper.convertValue(defaultTemplate, JsonNode.class));
         alert.setTemplateName("default");
 
-        //Alert persistedAlert = alertRepository.save(alert);
-        Alert persistedAlert= alert;
+        Alert persistedAlert = alertRepository.save(alert);
         assertNotNull(persistedAlert);
 
         ResponseEntity<Alert> responseEntity = this.restTemplate.getForEntity(
@@ -223,8 +182,7 @@ public class AlertControllerTest extends PostgresContainerBaseTest
         alert.setTemplateBody(mapper.convertValue(defaultTemplate, JsonNode.class));
         alert.setTemplateName("default");
 
-        //Alert persistedAlert = alertRepository.save(alert);
-        Alert persistedAlert = alert;
+        Alert persistedAlert = alertRepository.save(alert);
         assertNotNull(persistedAlert);
 
         String ldap0 = "foo42br";
@@ -234,98 +192,21 @@ public class AlertControllerTest extends PostgresContainerBaseTest
         UserAlert userAlert1 = new UserAlert(ldap1, persistedAlert.getId());
         userAlert1.setAlert(persistedAlert);
 
-        //UserAlert persistedUserAlert0 = userAlertRepository.save(userAlert0);
-        UserAlert persistedUserAlert0 = userAlert0;//Temporally Line
+        UserAlert persistedUserAlert0 = userAlertRepository.save(userAlert0);
         assertNotNull(persistedUserAlert0);
-        //UserAlert persistedUserAlert1 = userAlertRepository.save(userAlert1);
-        UserAlert persistedUserAlert1 = userAlert0;//Temporally Line
+        UserAlert persistedUserAlert1 = userAlertRepository.save(userAlert1);
         assertNotNull(persistedUserAlert1);
 
         this.restTemplate.delete("http://localhost:" + port + "/alert/" + persistedAlert.getId());
 
-        //List<Alert> deletedAlerts = alertRepository.findAllById(List.of(persistedAlert.getId()));
-        //assertEquals(0, deletedAlerts.size());
+        List<Alert> deletedAlerts = alertRepository.findAllById(List.of(persistedAlert.getId()));
+        assertEquals(0, deletedAlerts.size());
 
         List<UserAlertId> userAlertIds = List.of(
                 new UserAlertId(persistedUserAlert0.getLdap(), persistedUserAlert0.getAlertId()),
                 new UserAlertId(persistedUserAlert1.getLdap(), persistedUserAlert1.getAlertId())
         );
-        //List<UserAlert> deletedUserAlerts = userAlertRepository.findAllById(userAlertIds);
-        //assertEquals(0, deletedUserAlerts.size());
+        List<UserAlert> deletedUserAlerts = userAlertRepository.findAllById(userAlertIds);
+        assertEquals(0, deletedUserAlerts.size());
     }
-
-    @Test
-    @DisplayName("SaveAlertsByDCS")
-    void generateAlertByDCS() throws Exception {
-        final String dcs = "001-001-001";
-        CreateAlertRequest alertRequest = new CreateAlertRequest();
-        alertRequest.setType("Regional Assortment");
-        alertRequest.setSystemSource("My Assortment");
-        alertRequest.setExpirationDate(null);
-        alertRequest.setKeyIdentifiers(null);
-        Map<String, String> keyIdentifiers = new HashMap<>();
-        keyIdentifiers.put("sku", "123456");
-        keyIdentifiers.put("cpi", "0.98");
-        alertRequest.setKeyIdentifiers(keyIdentifiers);
-        alertRequest.setTemplateName("default");
-        Map<String, String> defaultTemplate = new HashMap<>();
-        defaultTemplate.put("title","title");
-        defaultTemplate.put("titleDescription","title description");
-        defaultTemplate.put("primaryText1","primary text 1");
-        defaultTemplate.put("primaryText2","primary text 2");
-        defaultTemplate.put("tertiaryText","tertiary text");
-        defaultTemplate.put("primaryLinkText","link");
-        defaultTemplate.put("primaryLinkUri","http://localhost:8080");
-        alertRequest.setTemplateBody(new ObjectMapper().convertValue(defaultTemplate, HashMap.class));
-        alertRequest.setTemplateName("default");
-
-        when(alertService.createAlertWithLdapAssociations(any(), List.of(anyString())))
-                .thenReturn(alertRequest.toAlert());
-        System.out.println(alertRequest);
-        ValidationException AL= new ValidationException("apple");
-        System.out.println(AL.getStatus());
-        final String SaveBy_DCS_URL="/alert/dcs/";
-
-        //1°Test SaveSuccess
-        mvc.perform(MockMvcRequestBuilders
-                        .post( SaveBy_DCS_URL+ dcs)
-                        .content(mapper.writeValueAsBytes(alertRequest))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isCreated());
-
-        //2°Test MissingDCS
-        mvc.perform(MockMvcRequestBuilders
-                        .post( SaveBy_DCS_URL)
-                        .content(mapper.writeValueAsBytes(alertRequest))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(405));
-
-        //3° MissingBody
-        mvc.perform(MockMvcRequestBuilders
-                        .post( SaveBy_DCS_URL+ dcs)
-                        .content(mapper.writeValueAsBytes(null))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(400));
-
-        //4°Test WrongSize DCS
-        mvc.perform(MockMvcRequestBuilders
-                        .post( SaveBy_DCS_URL+ "0")
-                        .content(mapper.writeValueAsBytes(alertRequest))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(409));
-
-        //5°Test Zero DCS
-        mvc.perform(MockMvcRequestBuilders
-                        .post( SaveBy_DCS_URL+ "000-000-000")
-                        .content(mapper.writeValueAsBytes(alertRequest))
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().is(406));
-    }
-
 }
-
