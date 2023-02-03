aaf39715ff43557459a4623ab24d100515468244
Pradhan, Praney <PRANEY_PRADHAN@homedepot.com>
2023-02-03 11:46:16 -0600

    Update WebConfig.java

--- src/main/java/com/homedepot/mm/pc/merchantalerting/configuration/WebConfig.java
+++ src/main/java/com/homedepot/mm/pc/merchantalerting/configuration/WebConfig.java
@@ -2,27 +2,18 @@ package com.homedepot.mm.pc.merchantalerting.configuration;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-public class CORSPathConfig {
+public class WebConfig implements WebMvcConfigurer{
     @Value("${cors.allowed.origins}")
     private String[] allowedOrigins;
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-
-        //String allowedDomains = "https://" + "*" + allowedOrigins;
-
-        return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
                             .allowedOrigins(allowedOrigins)
                             .allowedMethods("*")
                             .allowedHeaders("*");
-            }
-        };
     }
 }
