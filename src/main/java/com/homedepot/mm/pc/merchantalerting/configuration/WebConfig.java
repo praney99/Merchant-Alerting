2550774e1bb4f522c040ae86b6b08322178026c9
Pradhan, Praney <PRANEY_PRADHAN@homedepot.com>
2023-02-07 07:39:22 -0600

    Duplicates removed.

--- src/main/java/com/homedepot/mm/pc/merchantalerting/configuration/WebConfig.java
+++ src/main/java/com/homedepot/mm/pc/merchantalerting/configuration/WebConfig.java
@@ -8,12 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer{
     @Value("${cors.allowed.origins}")
     private String[] allowedOrigins;
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                    registry.addMapping("/**")
-                            .allowedOrigins(allowedOrigins)
-                            .allowedMethods("*")
-                            .allowedHeaders("*");
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("*")
+                .allowedHeaders("*");
     }
 }
